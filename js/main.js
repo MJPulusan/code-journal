7a3148774f40a5e442ab0ace96ae41956a866cb3
Michael Pulusan <mjpulusan@yahoo.com>
2025-02-25 18:56:19 +0000

    done HTML appropriate responsive layout

--- js/main.js
+++ js/main.js
@@ -1,27 +1,21 @@
-"use strict";
-const form = document.getElementById('photo-form');
+'use strict';
 const $photoURLInput = document.querySelector('#photoURL');
 const $photoPreview = document.querySelector('#photo-preview');
-if (!$photoURLInput)
-    throw new Error('$photoURLInput does not exist');
-if (!$photoPreview)
-    throw new Error('$photoPreview does not exist');
-const dataModel = {
-    nextEntryId: 1,
-    entries: [],
-};
+const form = document.getElementById('photo-form');
+if (!$photoURLInput) throw new Error('$photoURLInput does not exist');
+if (!$photoPreview) throw new Error('$photoPreview does not exist');
 $photoURLInput.addEventListener('input', () => {
-    $photoPreview.src = $photoURLInput.value;
+  $photoPreview.src = $photoURLInput.value;
 });
-form?.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const newEntry = {
-        entryId: dataModel.nextEntryId,
-        photoURL: $photoURLInput.value,
-    };
-    dataModel.nextEntryId++;
-    dataModel.entries.unshift(newEntry);
-    console.log(dataModel);
-    form.reset();
-    $photoPreview.src = 'placeholder.png';
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const newEntry = {
+    entryId: data.nextEntryId,
+    photoURL: $photoURLInput.value,
+  };
+  data.nextEntryId++;
+  data.entries = [newEntry].concat(data.entries);
+  console.log(data);
+  form.reset();
+  $photoPreview.src = 'images/placeholder-image-square.jpg';
 });
--- ts/main.ts
+++ ts/main.ts
@@ -1,33 +1,28 @@
-const form = document.getElementById('photo-form') as HTMLFormElement;
 const $photoURLInput = document.querySelector('#photoURL') as HTMLInputElement;
-const $photoPreview = document.querySelector('#photo-preview') as HTMLImageElement;
+const $photoPreview = document.querySelector(
+  '#photo-preview',
+) as HTMLImageElement;
+const form = document.getElementById('photo-form') as HTMLFormElement;
 
 if (!$photoURLInput) throw new Error('$photoURLInput does not exist');
 if (!$photoPreview) throw new Error('$photoPreview does not exist');
-
-const dataModel = {
-  nextEntryId: 1,
-  entries: [],
-};
 
 $photoURLInput.addEventListener('input', () => {
   $photoPreview.src = $photoURLInput.value;
 });
 
-form?.addEventListener('submit', (event: Event) => {
+form.addEventListener('submit', (event: Event) => {
   event.preventDefault();
 
+  const newEntry = {
+    entryId: data.nextEntryId,
+    photoURL: $photoURLInput.value,
+  };
 
-const newEntry = {
-  entryId: dataModel.nextEntryId,
-  photoURL: $photoURLInput.value,
-};
+  data.nextEntryId++;
+  data.entries = [newEntry].concat(data.entries);
+  console.log(data);
 
-dataModel.nextEntryId++;
-dataModel.entries.unshift(newEntry);
-console.log(dataModel);
-form.reset();
-$photoPreview.src = 'placeholder.png';
-
-
+  form.reset();
+  $photoPreview.src = 'images/placeholder-image-square.jpg';
 });
