9c75694471d2e4acf756f9b617d5adc6e607c4b5
Michael Pulusan <mjpulusan@yahoo.com>
2025-02-26 17:34:45 +0000

    Updated user-can-create-an-entry

--- ts/data.ts
+++ ts/data.ts
@@ -5,71 +5,18 @@ interface Data {
   nextEntryId: number;
 }
 
-const data: Data = {
-  view: 'entry-form',
-  entries: [],
-  editing: null,
-  nextEntryId: 1,
-};
+const data = readData();
 
 function writeData(): void {
   const dataJSON = JSON.stringify(data);
   localStorage.setItem('data-storage', dataJSON);
 }
 
-function readData(): [] {
+function readData(): Data {
   const dataJSON = localStorage.getItem('data-storage');
   if (dataJSON) {
     return JSON.parse(dataJSON);
   } else {
-    return [];
+    return { view: 'entry-form', entries: [], editing: null, nextEntryId: 1 };
   }
 }
-
-//  const STORAGE_KEY = 'entryData';
-
-// function saveData(): void {
-//   localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
-// }
-
-// function loadData(): Data {
-//   const storedData = localStorage.getItem(STORAGE_KEY);
-//   return storedData
-//     ? JSON.parse(storedData)
-//     : {
-//         view: 'entry-form',
-//         entries: [],
-//         editing: null,
-//         nextEntryId: 1,
-//       };
-// }
-
-// function addEntry(title: string, photoURL: string, notes: string): void {
-//   const newEntry: Entry = {
-//     entryId: data.nextEntryId++,
-//     title,
-//     photoURL,
-//     notes,
-//   };
-
-//   data.entries.push(newEntry);
-//   saveData();
-// }
-
-// function setView(view: string): void {
-//   data.view = view;
-//   saveData();
-// }
-
-// function setEditing(entry: null): void {
-//   data.editing = entry;
-//   saveData();
-// }
-
-// // import('./data').then(
-// //   ({ saveData, loadData, addEntry, setView, setEditing }) => {
-// //     addEntry($titleInput.value, $photoURLInput.value, $notes.value);
-// //   },
-// // );
-
-// export { data, saveData, loadData, addEntry, setView, setEditing };
